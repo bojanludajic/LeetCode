bd9b886c12c601764b7728853ab4f224ef7e8241
bojan.ludajic <bojan.ludajic@gmail.com>
2024-07-16 13:55:45 +0200

    Solved Search Insert Position and First Bad Version in Kotlin

--- src/solutionskotlin/LC278.kt
+++ src/solutionskotlin/LC278.kt
@@ -1,2 +1,20 @@
 package solutionskotlin
 
+fun main() {
+
+}
+
+fun isBadVersion(version: Int) : Boolean {
+    return false
+}
+
+fun firstBadVersion(n: Int) : Int {
+    var start = 1
+    var end = n
+    while(start <= end) {
+        val mid = start + (end - start) / 2
+        if(isBadVersion(mid)) end = mid - 1
+        else start = mid + 1
+    }
+    return start
+}
