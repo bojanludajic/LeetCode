1f24ebbc07bcadd98cb590d6354ca9ea1ec089bd
bojan.ludajic <bojan.ludajic@gmail.com>
2024-07-20 18:39:45 +0200

    Solved task 334 in Kotlin (LeetCode75 task 8/75)

---
+++ src/solutionskotlin/LC334.kt
@@ -0,0 +1,18 @@
+package solutionskotlin
+
+fun main() {
+    val nums = intArrayOf(20, 100, 10, 12, 5, 13)
+}
+
+fun increasingTriplet(nums: IntArray): Boolean {
+    var first = Integer.MAX_VALUE
+    var second = Integer.MAX_VALUE
+    for(num in nums) {
+        when  {
+            num <= first -> first = num
+            num <= second -> second = num
+            else -> return true
+        }
+    }
+    return false
+}
