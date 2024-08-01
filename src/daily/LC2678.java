d121ce953d22ac880b41986710e106f4c3ce2e56
Bojan Ludajic <bojanludajic@Bojans-MacBook-Air.local>
2024-08-01 15:53:59 +0200

    solved task 2678 in java and kotlin - daily for 1 aug 2024

--- src/daily/LC2678.java
+++ src/daily/LC2678.java
@@ -1,2 +1,18 @@
-package daily;public class LC2678 {
+package daily;
+
+public class LC2678 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int countSeniors(String[] details) {
+        int count = 0;
+        for (String detail : details) {
+            if(Integer.parseInt(detail.substring(10,11)) > 60) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
--- src/solutionskotlin/LC2678.kt
+++ src/solutionskotlin/LC2678.kt
@@ -1,2 +1,6 @@
 package solutionskotlin
 
+fun countSeniors(details: Array<String>): Int {
+    return details.filter { it.substring(11, 13).toInt() > 60}
+        .count()
+}
